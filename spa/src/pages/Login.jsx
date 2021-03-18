9043ca735df73b4692c41ac34e4e00a7194c317e
Casey James Perno <casey@khaosophy.com>
2021-03-18 16:04:49 -0400

    added login form markup and styles

--- spa/src/pages/Login.jsx
+++ spa/src/pages/Login.jsx
@@ -1,5 +1,28 @@
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import '../styles/login.css';
+
 export default function Login() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submitting...');
+  }
+
   return (
-    <h1>You will log in here</h1>
+      <Form onSubmit={handleSubmit} className="login">
+        <h1>Log in</h1>
+        <p>{/* todo text */}</p>
+        <FormGroup>
+          <Label for="loginEmail">Email Address</Label>
+          <Input type="email" name="email" id="loginEmail" />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="loginPass">Password</Label>
+          <Input type="password" name="password" id="loginPass" />
+        </FormGroup>
+        
+        <Button>Log in</Button>
+      </Form>
   )
 }
