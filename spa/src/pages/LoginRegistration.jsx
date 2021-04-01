9c181a7f4d9843ad8cd4f1c0600dc664671d3096
Casey James Perno <casey@khaosophy.com>
2021-04-01 11:26:05 -0400

    combined Login and Registration components

--- spa/src/pages/LoginRegistration.jsx
+++ spa/src/pages/LoginRegistration.jsx
@@ -5,29 +5,40 @@ import { login } from '../utils/auth';
 import '../styles/login.css';
 import UserContext from '../context/user';
 
-export default function Login() {
+export default function LoginRegistration({ action }) {
   /* todo: combine this page with Registration for DRYness */
   const { setUser } = useContext(UserContext);
   const history = useHistory();
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
+  const [ confirmPass, setConfirmPass ] = useState('');
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+  const handleLogin = async () => {
     const user = await login({email, password});
     if(user) {
       console.log('login success!');
       setUser(user);
       history.push('/');
+      // todo: make this redirect more dynamic so that it goes where they wanted
     } else {
       console.error('failed to log in');
       // todo: better error UX
     }
   }
 
+  const handleRegistration = async () => {
+    console.log('registering...');
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(action === 'login') { handleLogin(); }
+    if(action === 'register') { handleRegistration(); }
+  }
+
   return (
-      <Form onSubmit={handleLogin} className="login">
-        <h1>Log in</h1>
+      <Form onSubmit={handleSubmit} className="login">
+        <h1>{action === 'login' ? 'Log in' : 'Register'}</h1>
         <p>{/* todo text */}</p>
         <FormGroup>
           <Label for="loginEmail">Email Address</Label>
@@ -36,6 +47,7 @@ export default function Login() {
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </FormGroup>
 
@@ -46,10 +58,23 @@ export default function Login() {
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
         </FormGroup>
+
+        {action === 'register' && (
+          <FormGroup>
+            <Label for="registerPass">Confirm Password</Label>
+            <Input 
+              id="registerPass"
+              type="password"
+              value={confirmPass}
+              onChange={(e) => setConfirmPass(e.target.value)}
+            />
+          </FormGroup>
+        )}
         
-        <Button>Log in</Button>
+        <Button>{action === 'login' ? 'Log in' : 'Register'}</Button>
       </Form>
   )
 }
