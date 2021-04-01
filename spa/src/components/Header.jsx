e0987bea4c0b5b55938f677d2c45971fa70c2d44
Casey James Perno <casey@khaosophy.com>
2021-04-01 11:08:25 -0400

    handle log out

--- spa/src/components/Header.jsx
+++ spa/src/components/Header.jsx
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom';
 import { Container, Nav, NavItem, NavLink } from 'reactstrap';
 import '../styles/header.css';
-// import { useContext } from 'react';
-// import UserContext from '../context/user';
+import { useContext } from 'react';
+import UserContext from '../context/user';
+import { logout } from '../utils/auth';
+import { useHistory } from 'react-router-dom';
 
 export default function Header() {
-  // const { user, setUser } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const history = useHistory();
+
+  const handleLogout = () => {
+    logout();
+    setUser(null);
+    history.push('/');
+  }
+
   return (
     <header>
       <Container>
@@ -17,18 +27,18 @@ export default function Header() {
           <NavItem>
             <NavLink tag={Link} to="/recipes">Recipes</NavLink>
           </NavItem>
-            {/* {(user) ? (
-              <li>
-                <Link to="/logout">Log out</Link>
-              </li>
-            ) : (<> */}
-              <NavItem>
-                <NavLink tag={Link} to="/login">Login</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} to="/register">Register</NavLink>
-              </NavItem>
-            {/* </>)} */}
+          {(user) ? (
+            <NavItem>
+              <NavLink tag={Link} onClick={handleLogout}>Log out</NavLink>
+            </NavItem>
+          ) : (<>
+            <NavItem>
+              <NavLink tag={Link} to="/login">Login</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/register">Register</NavLink>
+            </NavItem>
+          </>)}
         </Nav>
       </Container>
     </header>
